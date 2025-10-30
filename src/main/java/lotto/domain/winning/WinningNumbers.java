cb436f7894de5eabbdbfac663c0efa8452629c6a
dongheeHan <ark182818@gmail.com>
2025-10-30 22:14:50 +0900

    feat(domain): WinningNumbers 클래스 기본 구조 구현
    - 당첨 번호 6개를 저장하는 생성자 구현
    - contains() 메서드로 특정 번호 포함 여부 확인
    - Lotto의 countMatches() 메서드를 위한 협력 객체

---
+++ src/main/java/lotto/domain/winning/WinningNumbers.java
@@ -0,0 +1,15 @@
+package lotto.domain.winning;
+
+import java.util.List;
+
+public class WinningNumbers {
+    private final List<Integer> numbers;
+
+    public WinningNumbers(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+
+    public boolean contains(int number) {
+        return numbers.contains(number);
+    }
+}
