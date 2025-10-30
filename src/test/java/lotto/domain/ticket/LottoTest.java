36d572c9a9aba3650f5d2803ef2eea32b71a4a29
dongheeHan <ark182818@gmail.com>
2025-10-30 21:31:07 +0900

    test(domain): Lotto 생성 시 정상 케이스 테스트 추가
    - 6개의 번호로 로또 생성 테스트
    - @Nested로 생성 관련 테스트 그룹화
    - 기존 제공 테스트 유지

--- src/test/java/lotto/domain/ticket/LottoTest.java
+++ src/test/java/lotto/domain/ticket/LottoTest.java
@@ -1,5 +1,6 @@
-package lotto.domain.ticket;
+package lotto;
 
+import lotto.domain.ticket.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -7,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
 
@@ -26,5 +28,18 @@ class LottoTest {
             // then
             assertThat(lotto).isNotNull();
         }
+    }
+
+    @Test
+    void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
