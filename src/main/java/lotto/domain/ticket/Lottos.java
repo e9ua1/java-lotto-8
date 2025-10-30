dc422fe8dd3ca456c8fb901aa4a213a019c76e7a
dongheeHan <ark182818@gmail.com>
2025-10-30 22:28:00 +0900

    feat(domain): Lottos 클래스 기본 구조 구현
    - 로또 목록을 관리하는 일급 컬렉션
    - size() 메서드로 로또 개수 반환
    - LottoGenerator의 생성 결과를 담는 객체

---
+++ src/main/java/lotto/domain/ticket/Lottos.java
@@ -0,0 +1,15 @@
+package lotto.domain.ticket;
+
+import java.util.List;
+
+public class Lottos {
+    private final List<Lotto> lottos;
+
+    public Lottos(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+    public int size() {
+        return lottos.size();
+    }
+}
