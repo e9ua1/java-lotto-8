8fb84268af77fa6aa73c238913dc18fcaf3f8d8c
dongheeHan <ark182818@gmail.com>
2025-10-30 22:05:48 +0900

    feat(domain): Lotto 번호 범위 검증 로직 구현
    - 1~45 범위를 벗어나면 IllegalArgumentException 발생
    - validateRange 메서드 추가
    - 검증 로직을 각각의 메서드로 분리 (validateSize, validateDuplicate, validateRange)

--- src/main/java/lotto/domain/ticket/Lotto.java
+++ src/main/java/lotto/domain/ticket/Lotto.java
@@ -1,8 +1,13 @@
 package lotto.domain.ticket;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
+    private static final int LOTTO_NUMBER_SIZE = 6;
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,8 +16,26 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        validateSize(numbers);
+        validateDuplicate(numbers);
+        validateRange(numbers);
+    }
+
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_NUMBER_SIZE) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        if (numbers.size() != new HashSet<>(numbers).size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        if (numbers.stream().anyMatch(number -> number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
     }
 }
